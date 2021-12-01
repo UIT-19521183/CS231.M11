d49f6874788ac43250fdfb29af0b1e433e211be7
homyhanh <hanhmyho01@gmail.com>
2021-12-01 09:59:39 +0700

    Add read_data_train.py

---
+++ LBP/Read_data_train.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.externals import joblib
+import argparse
+from function import *
+
+parser = argparse.ArgumentParser(description='Parse Training Directory')
+parser.add_argument('--path', help='Path to directory contraining training images')
+
+args = parser.parse_args()
+train_dir = args.path
+pos_dir = os.path.join(train_dir, 'pos')
+neg_dir = os.path.join(train_dir, 'neg')
+
+X, y, pos_count, neg_count = read_images(pos_dir, neg_dir)
+X, y = np.array(X), np.array(y)
+X, y = shuffle(X, y, random_state=0)
+
+print('Number of positive images:', pos_count)
+print('Number of negative images:', neg_count)
