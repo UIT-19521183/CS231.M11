e01c4ece0964798dc442eb9ad664de9280556520
homyhanh <79818022+homyhanh@users.noreply.github.com>
2021-12-03 09:28:00 +0700

    Update Read_data_test.py

--- LBP/Read_data_test.py
+++ LBP/Read_data_test.py
@@ -11,8 +11,8 @@ parser.add_argument('--path', help='Path to directory contraining testing images
 
 args = parser.parse_args()
 test_dir = args.path
-pos_dir_test = os.path.join(train_dir, 'pos')
-neg_dir_test = os.path.join(train_dir, 'neg')
+pos_dir_test = os.path.join(test_dir, 'pos')
+neg_dir_test = os.path.join(test_dir, 'neg')
 
 pos_features, neg_features, pos_count_test, neg_count_test = read_images_test(pos_dir_test, neg_dir_test)
 pos_features, neg_features = np.array(pos_features), np.array(neg_features)
